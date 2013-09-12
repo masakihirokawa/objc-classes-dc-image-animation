40f0bb8dfdcd60c5584f9a4418c095f32e1e53ae
Masaki Hirokawa <info@dolice.net>
2013-09-12 15:16:48 +0900

    Fix class

--- DCImageAnimationSample/DCImageAnimationSample/Classes/DCImageAnimation.h
+++ DCImageAnimationSample/DCImageAnimationSample/Classes/DCImageAnimation.h
@@ -7,8 +7,8 @@
 
 #import <UIKit/UIKit.h>
 
-#define SECOND                  60.0f
-#define ANIM_FPS                24.0f
+#define SECOND                  60.0
+#define ANIM_FPS                24.0
 #define ANIM_IMAGE_X            0
 #define ANIM_IMAGE_Y            0
 #define ANIM_IMAGE_WIDTH        320
@@ -22,23 +22,23 @@
     id<DCImageAnimationDelegate> _dc_delegate;
 }
 
+#pragma mark - property
 @property (nonatomic, assign) id<DCImageAnimationDelegate> dc_delegate;
 @property (nonatomic, retain) UIImageView *frameAnimationImageView;
 @property (nonatomic, retain) UIImageView *timerAnimationImageView;
 
+#pragma mark - public method
 - (id)init;
 - (void)startFrameAnimating:(NSString *)animationType :(NSInteger)animationImageNum :(NSString *)animationImagePrefix :(NSInteger)animationRepeatNum;
 - (void)startTimerAnimating:(NSString *)animationType :(NSInteger)animationImageNum :(NSString *)animationImagePrefix :(BOOL)isInfiniteLoopAnimation;
-- (void)setFps:(float)fps;
+- (void)setFps:(CGFloat)fps;
 - (void)resetFps;
 - (void)setRectangle:(CGRect)rect;
 - (void)resetRectangle;
 
 @end
 
-#pragma mark delegate prottype
+#pragma mark - delegate method
 @protocol DCImageAnimationDelegate<NSObject>
 @optional
-//ここにデリゲートメソッドを指定します
-
 @end
